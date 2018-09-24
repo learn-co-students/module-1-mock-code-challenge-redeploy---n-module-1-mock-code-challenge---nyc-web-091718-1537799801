fee25bc37881e5f806aa93b824ae48720845354d
TurtleFeeder <t.choong@yahoo.com>
2018-09-24 12:01:21 -0400

    first mockup code challenge commit

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,49 @@
 class Startup
+  attr_accessor :name
+  attr_reader :founder, :domain
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find {|startup| startup.founder == founder_name}
+  end
+
+  def self.domains
+    self.all.map {|startup| startup.domain}
+  end
+
+  def initialize(founder, name, domain)
+    @founder = founder
+    @name = name
+    @domain = domain
+    self.class.all << self
+  end #end initialize method
+
+  def pivot(domain)
+    #how do i change the domain without doing self.domain = "domain"
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    new_contract = FundingRound.new(type, self, venture_capitalist, investment)
+  end #end sign_contract
+
+  def funding_rounds
+    FundingRound.all.select {|funding_round| funding_round.startup == self}
+  end
+
+  def num_funding_rounds
+    self.funding_rounds.length
+  end
+
+  def total_funds
+    self.funding_rounds.map{|funding_round| funding_round.investment}.reduce(:+)
+  end
+
+  def investors
+    #call funding_round helper method and then collect the funding_round.investor names
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("Joe", "Newcompany", "domain1" )
+s2 = Startup.new("Bob", "Anothercompany", "domain2" )
+
+v1 = VentureCapitalist.new("Steve", "no")
+v2 = VentureCapitalist.new("Anna", "yes")
+
+f1 = FundingRound.new("Angel", s1, v1, 20.0)
+f2 = FundingRound.new("Pre-seed", s1, v2, 110.0)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
