e6cce957ed53816b0e3fa1ee772a51336ff3683b
DDarrow123 <ddarrowphl@gmail.com>
2018-09-24 12:01:26 -0400

    Worked on mock code challenge

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class VentureCapitalist
+
+  attr_reader :name, :trés_commas_club
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+
+  end
+
+#   def tres_commas(trés_commas_club)
+#     new_club = []
+#   self.all.select do |venture_capitalist|
+#     if venture_capitalist.include?(trés_commas_club)
+#       new_club << venture_capitalist
+#       binding.pry
+#     end
+#   end
+# end
+#
+#   def self.tres_commas_club
+#     return new_club
+#
+#   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,24 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+s1 = Startup.new("John Hill", "The Startr")
+s2 = Startup.new("Sarah Wilson", "The Endr")
+s3 = Startup.new("Luke Robertson", "The Middlr")
+
+
+vc1 = VentureCapitalist.new("Mr. Smith")
+vc2 = VentureCapitalist.new("Ms. Harrison")
+vc3 = VentureCapitalist.new("Mr. Owen")
+
+fr1 = FundingRound.new("Angel", s2, vc2)
+fr2 = FundingRound.new("Pre-Seed", s3, vc3)
+fr3 = FundingRound.new("Seed", s2, vc3)
+fr4 = FundingRound.new("Series A", s1, vc1)
+r4 = FundingRound.new("Series A", s1, vc1)
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
