8d2c2e85650c0b47ef5489f1c1066c3068c9da4e
eric-klimowich <eklimowich@hotmail.com>
2018-09-24 12:04:11 -0400

    Yikes

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,15 @@
 class FundingRound
-
+  # - `FundingRound.all`
+  #   - returns all of the funding rounds
+  # - `FundingRound#startup`
+  #   - returns the startup object for that given funding round
+  #   - Once a funding round is created, I should not be able to change the startup
+  # - `FundingRound#venture_capitalist`
+  #   - returns the venture capitalist object for that given funding round
+  #   - Once a funding round is created, I should not be able to change the venture capitalist
+  # - `FundingRound#type`
+  #   - returns the type of funding round
+  #   - Examples include: Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
+  # - `FundingRound#investment`
+  #   - returns the amount invested during this funding round. This should be a float that is not be a negative number.
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,67 @@
 class Startup
 
+  attr_reader :founder_name, :domain_name
+  attr_accessor :startup_name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(startup_name, founder_name, domain_name)
+    @startup_name = startup_name
+    @founder_name = founder_name
+    @domain_name = domain_name
+
+    @@all << self
+  end
+
+  def name
+    self.startup_name
+  end
+
+  def founder
+    self.founder_name
+  end
+
+  def domain
+    self.domain_name
+  end
+
+  def self.find_by_founder(name)
+    self.all.find do |company|
+      company.founder = name
+    end
+  end
+
+  def self.domains
+    dms = []
+    self.all.map do |dom|
+      dms << dom.domain
+    end
+    dms
+  end
+
+#`Startup#name` - returns a **string** that is the startup's name
+#`Startup#founder` - returns a **string** that is the founder's name - Once a startup is created, the founder cannot be changed.
+#`Startup#domain` - returns a **string** that is the startup's domain - Once a startup is created, the domain cannot be changed.
+#`Startup.all` - should return **all** of the startup instances
+#   - `Startup.find_by_founder`
+#     - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+#   - `Startup.domains`
+#     - should return an **array** of all of the different startup domains
+#   - `Startup.pivot(domain)`
+#     - given a string of a domain, change the domain of the startup
+
+# <!-- - `Startup#sign_contract(venture_capitalist, type, investment)`
+#   - given a **venture capitalist object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+# - `Startup#num_funding_rounds`
+#   - Returns the total number of funding rounds that the startup has gotten
+# - `Startup#total_funds`
+#   - Returns the total sum of investments that the startup has gotten
+# - `Startup#investors`
+#   - Returns a **unique** array of all the venture capitalists that have invested in this company
+# - `Startup#big_investors`
+#   - Returns a **unique** array of all the venture capitalists that have invested in this company and are in the Trés Commas club -->
 end
